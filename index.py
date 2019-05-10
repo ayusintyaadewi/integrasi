e0125dc998be5333d4df145af8c1e5ae7c7dcf6e
ayusintyaadewi <sintya_dewi2203@yahoo.com>
2019-05-10 20:18:25 +0800

    IMS Python Using Scalingo

--- index.py
+++ index.py
@@ -1,12 +1,25 @@
+import os
+from flask import Flask
 from pymongo import MongoClient
 from bson.json_util import dumps
 
-client = MongoClient("mongodb+srv://ayusintyaadewi:sintya2203*@cluster0-mdyzg.gcp.mongodb.net/test?retryWrites=true")
+app = Flask(__name__)
 
-db = client.mahasiswa
-collection = db.mahasiswa
-cursor = collection.find({})
 
-for mahasiswa in cursor:
-    print(dumps(mahasiswa))
-    print("\n")
+@app.route("/")
+def main():
+    client = MongoClient(
+        "mongodb+srv://ayusintyaadewi:sintya2203*@cluster0-mdyzg.gcp.mongodb.net/test?retryWrites=true")
+
+    db = client.mahasiswa
+    collection = db.mahasiswa
+    cursor = collection.find({})
+
+    for mahasiswa in cursor:
+        print(dumps(mahasiswa))
+        print("\n")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
