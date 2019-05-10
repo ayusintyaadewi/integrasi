d6a7cd9c3d6dec433cf97df7a9364ec53e0b2b2f
ayusintyaadewi <sintya_dewi2203@yahoo.com>
2019-05-10 19:47:10 +0800

    IMS Python Using Scalingo

---
+++ index.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+client = MongoClient("mongodb+srv://ayusintyaadewi:sintya2203*@cluster0-mdyzg.gcp.mongodb.net/test?retryWrites=true")
+
+db = client.mahasiswa
+collection = db.mahasiswa
+cursor = collection.find({})
+
+for mahasiswa in cursor:
+    print(dumps(mahasiswa))
+    print("\n")
